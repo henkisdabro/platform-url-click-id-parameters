6c517b9435a8277773db84a469984bf3471d5154
Henrik Söderlund <github@henriksoderlund.com>
2022-05-25 15:32:48 +0800

    Create parameter_array.js

---
+++ parameter_array.js
@@ -0,0 +1,26 @@
+[
+  'gclid',                 // Google Ads click ID - generic non-iOS
+  'gbraid',                // Google Ads click ID - iOS app to app measurement
+  'wbraid',                // Google Ads click ID - iOS app to web measurement
+  'gclsrc',                // Google Search Ads 360 click ID
+  'dclid',                 // Google Display click ID (Enhanced Attribution)
+  'fbclid',                // Meta click ID
+  'dicbo',                 // Outbrain click ID
+  'tblci',                 // Taboola click ID
+  'li_fat_id',             // Linkedin click ID
+  'rdt_cid',               // Reddit click ID
+  'ttclid',                // TikTok click ID
+  'twclid',                // Twitter click ID
+  'auctid',                // Teads click ID
+  'vmcid',                 // Yahoo DSP click ID
+  'afid',                  // Yahoo Native parameters
+  'subafid',
+  'msclkid',               // Microsoft click ID
+  'campaign',              // Microsoft campaign parameters
+  'mc_cid',                // Mailchimp click ID
+  'mc_eid',                // Mailchimp click ID
+  '_z1_agid',              // Zemanta click ID
+  '_z1_caid',              // Zemanta click ID
+  '_z1_msid',              // Zemanta click ID
+  '_z1_pub'                // Zemanta click ID
+]
