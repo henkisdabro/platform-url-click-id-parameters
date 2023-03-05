de8eb154655b70de7e7c040f6155c92232ed1ac0
Henrik Soederlund <github@henriksoderlund.com>
2023-03-05 18:17:42 +0800

    change to exacting a table from readme github action

--- generate-csv.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const parameters = require('./parameter_array');
-
-const data = parameters.map(param => ({ parameter: param }));
-const csv = data.map(item => item.parameter).join(',');
-
-fs.writeFileSync('parameters.csv', csv);
