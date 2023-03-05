8740cb11b6f748ad5d209d3bd44a9a41767f71fe
Henrik Soederlund <github@henriksoderlund.com>
2023-03-05 14:36:38 +0800

    iterate version

--- generate-csv.js
+++ generate-csv.js
@@ -1,7 +1,11 @@
 const fs = require('fs');
 
-const data = require('./parameter_array.js').data;
-const header = Object.keys(data[0]).join(',') + '\n';
-const rows = data.map(obj => Object.values(obj).join(',') + '\n').join('');
+const parameters = require('./parameter_array');
 
-fs.writeFileSync('./parameter_commaseparated.csv', header + rows);
+const data = parameters.map(param => ({ parameter: param }));
+
+const header = Object.keys(data[0]).join(',') + '\n';
+const rows = data.map(row => Object.values(row).join(',') + '\n').join('');
+
+fs.writeFileSync('parameters.csv', header + rows);
+console.log('parameters.csv generated!');
