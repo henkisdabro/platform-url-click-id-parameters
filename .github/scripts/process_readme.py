47dde299edf59e8760f2501f69da65d1b732df52
Henrik Soederlund <github@henriksoderlund.com>
2023-04-07 12:48:51 +0800

    flavor none

--- .github/scripts/process_readme.py
+++ .github/scripts/process_readme.py
@@ -3,7 +3,7 @@ import pandas as pd
 with open('README.md', 'r') as file:
     data = file.read()
 # Find the third markdown table
-tables = pd.read_html(data, flavor='github')
+tables = pd.read_html(data, flavor='None')
 third_table = tables[2]
 # Extract the values of the first column and create a comma-separated list
 first_column_values = third_table.iloc[:, 0].tolist()
